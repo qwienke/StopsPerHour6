f2c9f27d6e007ee2e6b7f84e5d81c5c16b2bf09c
qwienke <qwienke13@gmail.com>
2023-12-20 14:04:52 -0700

    Initial Commit

---
+++ StopsPerHour6 Watch App/StopsPerHour6App.swift
@@ -0,0 +1,17 @@
+//
+//  StopsPerHour6App.swift
+//  StopsPerHour6 Watch App
+//
+//  Created by Quinn Wienke on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct StopsPerHour6_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
